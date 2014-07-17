2b5fd4da1ceefec748419ff30a03527d7c7e792a
Paul Wenig <pwenig@gmail.com>
2014-07-17 10:58:19 -0600

    Added login error messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
-    if user
+    @user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
+    if @user
       session[:user_id] = user.id
       redirect_to '/products'
     else
       flash.now[:error] = "Invalid Email and Password Combination"
-      render "new"
+      render :new
     end
   end
 
