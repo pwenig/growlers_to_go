2d5ce7d1adcde9f048e6484f309b10e286485380
Paul Wenig <pwenig@gmail.com>
2014-07-17 19:09:41 -0600

    Style changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
     if @user
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect_to '/products'
     else
       flash.now[:error] = "Invalid Email and Password Combination"
